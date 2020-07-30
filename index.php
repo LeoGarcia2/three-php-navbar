6ea4e4cbce6b2998c09517bc12acd218aa6934b3
leogarcia2 <leogarcia2@outlook.fr>
2020-07-30 18:37:17 +0200

    v1 done

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ if (isset($_GET['logout'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Développeur Freelance à votre service !">
 
 
     <title>Three JS Navbar</title>
